f94239d3bcc485775ade9f917ff9fbadb9c94023
Lostceleste <inymtern_cl@126.com>
2023-08-26 00:38:09 +0800

    first commit

---
+++ src/main/java/com/after/life/gateway/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.after.life.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Author Lostceleste
+ * @Version 1.0
+ * @Date 2023-08-14 20:24
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("http://127.0.0.1:8800");
+        config.addAllowedOrigin("http://127.0.0.1:8801");
+        config.addAllowedOrigin("http://127.0.0.1:5173");
+        config.addAllowedOrigin("http://127.0.0.1:5174");
+        config.addAllowedHeader("*");
+        config.setMaxAge(18000L);
+        config.setAllowCredentials(false);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+
+}
